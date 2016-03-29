fd7d1e403d237b7e33616ae42908267e6fee8def
Matias Hernandez <matiasfh@gmail.com>
2016-03-29 11:05:24 -0300

    add documentation comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import Immutable from 'immutable'
 
+/**
+* Defaults arguments used for the HOC
+* @property {function} authSelector	- A function to get the authenticated status of
+*	the user from the app state
+* @property {string} redirectOnFailure - A string to identify the route used to redirect
+*	when no authenticated user
+*/
 const defaults = {
+	/**
+	* Get the authenticated status of the user
+	*/
 	authSelector: (authData: Object): boolean => {
 		if (Immutable.Map.isMap(authData)) {
 			return authData.get('authenticated')
@@ -12,11 +22,25 @@ const defaults = {
 	redirectOnFailure: '/login',
 }
 
+/**
+* The HOC definition
+* @param {object} args - An object with the basic definition used in the HOC
+* @property {function} authSelector
+* @property {string} redirectOnFailure
+*/
 export const AuthContainer = (args: Object): Function => {
 	const { authSelector, redirectOnFailure } = { ...defaults, ...args }
 
+	/**
+	* Allow to know if the user is authenticated
+	* based on the authSelector defined in the args
+	*/
 	const isAuthenticated = (state: Object): boolean => authSelector(state)
 
+	/**
+	* Check the authenticated state and redirect to some route if
+	* is not logged in
+	*/
 	const checkAuth = (state: Object, router: Object) => {
 		if (!isAuthenticated(state)) {
 			router.replace({
@@ -27,7 +51,14 @@ export const AuthContainer = (args: Object): Function => {
 		}
 	}
 
+	/**
+	* Perform the composition of a Component adding a way to check the authenticated status
+	* @params {React.Component} ComposedComponent - The base component to be composed
+	*/
 	const composed = (ComposedComponent: React.Component): React.Component => {
+		/**
+		* Used with redux.connect inject a state property into the component props
+		*/
 		function mapStateToProps(state: Object): Object {
 			if (Immutable.Map.isMap(state)) {
 				return { user: state.get('user') }
@@ -62,7 +93,14 @@ export const AuthContainer = (args: Object): Function => {
 		return Composed
 	}
 
-	composed.onEnter = (store: Object, nextState: Object, replace: Object) => {
+	/**
+	* A static property to be used as onEnter prop for <Route/>
+	* check the authenticated status in the route transition
+	* @param {object} store - The redux store
+	* @param {object} nextState -  The next state of the app
+	* @param {function} replace -  A reference to the replace function of route
+	*/
+	composed.onEnter = (store: Object, nextState: Object, replace: Function) => {
 		const state = store.getState()
 		let user
 		if (Immutable.Map.isMap(state)) {
@@ -70,7 +108,6 @@ export const AuthContainer = (args: Object): Function => {
 		} else {
 			user = state.user
 		}
-
 		const authData = authSelector(user)
 		checkAuth(authData, { replace })
 	}
